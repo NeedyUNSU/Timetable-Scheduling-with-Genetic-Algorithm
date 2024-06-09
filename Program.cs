ff3c89173786ede1e3a9c34e9ed50fd1c30d0404
NeedyUNSU <72944930+NeedyUNSU@users.noreply.github.com>
2024-06-09 15:49:41 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,13 +5,20 @@
         public string? Title { get; set; }
         public string? Teacher { get; set; }
         public int? Classroom { get; set; }
+        public int? inMinimumWeek { get; set; }
+        public int? inMaximunDay { get; set; }
+
+        public int? inMiddleMAX { get; set; } = 0;
         public bool isFree { get; set; } = true;
 
-        public Lesson(string title, string teacher, int classroom, bool active = true)
+        public Lesson(string title, string teacher, int classroom, int inminimumweek, int inmaximumday, int inmiddlemax = 0, bool active = true)
         {
             Title = title;
             Teacher = teacher;
             Classroom = classroom;
+            inMinimumWeek = inminimumweek;
+            inMinimumWeek = inmaximumday;
+            inMiddleMAX = inmiddlemax;
             isFree = active;
         }
 
@@ -25,7 +32,19 @@
     {
         public List<string> ClassName { get; set; } = new List<string>() { "Klasa 1", "Klasa 2", "Klasa 3", "Klasa 4" };
 
-        public Lesson[] Monday { get; set; } = new Lesson[] { };
+        public List<Lesson> Monday { get; set; } = new List<Lesson>();
+        public List<Lesson> Tuesday { get; set; } = new List<Lesson>();
+        public List<Lesson> Wednesday { get; set; } = new List<Lesson>();
+        public List<Lesson> Thursday { get; set; } = new List<Lesson>();
+        public List<Lesson> Friday { get; set; } = new List<Lesson>();
+        
+
+    }
+
+    public class Schedule
+    {
+        public List<ScheduleOne> Classes { get; set; }
+
 
     }
 
@@ -35,7 +54,6 @@
         {
             ScheduleOne test = new ScheduleOne();
 
-            Console.WriteLine(test.Monday[2][0]);
 
         }
     }
